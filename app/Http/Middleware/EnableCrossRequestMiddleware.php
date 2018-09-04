c63f28979f768e3e5127743d07d701f3706b9257
chaishubin <chaishubin@huobanys.com>
2018-09-04 13:22:43 +0800

    chai kusyu

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -24,7 +24,7 @@ class EnableCrossRequestMiddleware
             'http://ceshih5.honour.huobanys.cn'
         ];
         if (in_array($origin, $allow_origin)) {
-            $response->header('Access-Control-Allow-Origin', 'http://localhost:8080');
+//            $response->header('Access-Control-Allow-Origin', $origin);
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
             $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
             $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
