7245441cc444f28288775d3e871ef6c407ef7593
chaishubin <chaishubin@huobanys.com>
2018-09-27 17:26:03 +0800

    chai log

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -38,10 +38,10 @@ class VoteController extends Controller
             $check_expert = $this->checkExpert($voters['phone_number']);
             $voters_type = $check_expert === 'pass' ? 2 : 1; //pass是专家2，否则就是大众1
 
-            $vote_relation = VoteRelationModel::where('voters_id',$voters['id'])->get();
-            Log::info('$vote_relation'.$vote_relation);
             if ($voters_type == 1){ // 大众
-                if ($vote_relation){ // 若查询到记录，说明该用户已经至少投过一次票了，普通用户只能参与投票一次
+                $vote_user_relation = VoteRelationModel::where('voters_id',$voters['id'])->get();
+                Log::info('$vote_relation'.$vote_user_relation);
+                if ($vote_user_relation){ // 若查询到记录，说明该用户已经至少投过一次票了，普通用户只能参与投票一次
                     return Common::jsonFormat('500','您只能投一票，您已经参与过投票了');
                 }
             }else{ // 专家
