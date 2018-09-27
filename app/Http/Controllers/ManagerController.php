51bd8e309ac06814754cdc5e407708f8510d80a9
chaishubin <chaishubin@huobanys.com>
2018-09-27 10:03:43 +0800

    chai review change home

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -22,6 +22,7 @@ use App\Models\Manager\ManagerModel;
 use App\Models\Manager\SettingModel;
 use App\Models\Manager\SignUpInfoReview;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 use Psy\TabCompletion\Matcher\CommandsMatcher;
