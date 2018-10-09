cec183712b64d338f4864c21453db97474510024
zhaopeng <18204869192@163.com>
2018-10-09 17:53:22 +0800

    增加管理员编辑账号校验是否存在

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -107,10 +107,15 @@ class ManagerController extends Controller
                 return Common::jsonFormat('500','此管理员不存在哟');
             }
 
+
             if (isset($info['nickname']) && !is_null($info['nickname'])){
                 $manager->nickname = $info['nickname'];
             }
             if (isset($info['account']) && !is_null($info['account'])){
+                $result = ManagerModel::query()->where('account',$info['account'])->where('id','!=',$info['id'])->first();
+                if ($result){
+                    return Common::jsonFormat('500','该管理员账号已存在');
+                }
                 $manager->account = $info['account'];
             }
             if (isset($info['password']) && !is_null($info['password'])){
